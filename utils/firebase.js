7c58c31ae4d1d8a847b9c364c2be0adb9fdbac14
fuad mahmud <45328705+fuadmahmud@users.noreply.github.com>
2021-12-08 14:58:58 +0700

    remove api key

--- utils/firebase.js
+++ utils/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "AIzaSyAdkG_HXB7IsG0PFpv0DqSja-PKtmssyDc",
+  apiKey: "",
   authDomain: "covid19-statistic.firebaseapp.com",
   projectId: "covid19-statistic",
   storageBucket: "covid19-statistic.appspot.com",
   messagingSenderId: "103901004934",
   appId: "1:103901004934:web:2c62eeb7a5ae31e445bd07",
-  measurementId: "G-61W68XVLNS"
+  measurementId: ""
 };
 
 if (!firebase.apps.length) {
